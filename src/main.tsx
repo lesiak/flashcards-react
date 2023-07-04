f70a2519015f41580c89485533bc1120e27db0b1
lesiak <lesiak@outlook.com>
2023-07-04 22:50:59 +0200

    Use @fluentui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {FluentProvider, teamsLightTheme} from '@fluentui/react-components';
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={teamsLightTheme}>
+      <App/>
+    </FluentProvider>
   </React.StrictMode>,
 )
