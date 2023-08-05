4d6c21335f474fbf5c2694e58ab12ee76388af1d
lesiak <lesiak@outlook.com>
2023-08-05 10:10:01 +0200

    Load all decks on startup

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,39 @@ import {Dictionary} from './Dictionary';
 import {Card} from "./model/Card.ts";
 import {LanguageContext} from "./context/LanguageContext.tsx";
 import {NavBar} from './NavBar.tsx';
-import { HomePage } from './HomePage.tsx';
+import {HomePage} from './HomePage.tsx';
+import {LanguageInfo} from './model/LanguageInfo.ts';
 
 const deckNames = [
   '01_NatureBeginner',
   '02_Numbers1to5'
 ]
 
-
-
 async function loadDeck(lang: string, deckName: string): Promise<Card[]> {
-  const resp = await fetch(`wordfiles/${lang}/${deckName}.json`);
+  const deckUrl = `wordfiles/${lang}/${deckName}.json`;
+  const resp = await fetch(deckUrl);
   if (!resp.ok) {
-    throw new Error('Cannot load deck');
+    throw new Error(`Cannot load deck ${deckUrl}`);
   }
   return await resp.json() as Card[];
 }
 
+function loadAllDecksIgnoringErrors(lang: LanguageInfo, deckNames: string[]): Promise<Card[][]> {
+  const loadDeckIgnoringErrors = (deckName: string) => loadDeck(lang.code, deckName)
+    .catch(e => {
+      console.log('AAA', e);
+      return [];
+    });
+
+  const loadTasks = deckNames.map(loadDeckIgnoringErrors);
+  return Promise.all(loadTasks);
+}
+
 function App() {
   const {currentLanguage} = useContext(LanguageContext);
-  const [deck, setDeck] = useState([] as Card[])
-  const deckName = deckNames[0];
+  const [_, setDeck] = useState([] as Card[])
+  const [allCards, setAllCards] = useState([] as Card[])
+  const deckIdx = 0;
   const [selectedTab, setSelectedTab] = useState<TabValue>("homeTab");
 
   const onTabSelect = (_: SelectTabEvent, data: SelectTabData) => {
@@ -40,7 +52,7 @@ function App() {
     async function getUserInfo() {
       const response = await fetch('/.auth/me');
       const payload = await response.json();
-      const { clientPrincipal } = payload;
+      const {clientPrincipal} = payload;
       return clientPrincipal;
     }
 
@@ -50,19 +62,21 @@ function App() {
   }, [])
 
   useEffect(() => {
-    loadDeck(currentLanguage.code, deckName).then(newDeck => setDeck(newDeck));
-  }, [currentLanguage.code])
 
+    loadAllDecksIgnoringErrors(currentLanguage, deckNames).then(values => {
+        const newAllCards = values.flatMap(v => v)
+        setAllCards(newAllCards);
+        setDeck(values[deckIdx]);
+      })
+    }, [currentLanguage.code])
 
+    return (
+      <>
+        <NavBar selectedTab={selectedTab} onTabSelect={onTabSelect}/>
+        {selectedTab === 'homeTab' && <HomePage/>}
+        {selectedTab === 'dictionaryTab' && <Dictionary cards={allCards} currentLanguage={currentLanguage}/>}
+      </>
+    )
+  }
 
-
-  return (
-    <>
-      <NavBar selectedTab={selectedTab} onTabSelect={onTabSelect} />
-      {selectedTab === 'homeTab' && <HomePage />}
-      {selectedTab === 'dictionaryTab' && <Dictionary cards={deck} currentLanguage={currentLanguage}/>}
-    </>
-  )
-}
-
-export default App
+  export default App
