f5c013c24a3277f5db2c8a44bbf671f1da7e22cd
lesiak <lesiak@outlook.com>
2023-08-07 22:30:19 +0200

    Filtering in Dictionary

--- src/Dictionary.tsx
+++ src/Dictionary.tsx
@@ -1,40 +1,58 @@
+import React, {useState} from 'react';
+
 import {
   TableBody,
   TableCell,
   TableRow,
   Table,
   TableHeader,
-  TableHeaderCell,
+  TableHeaderCell, Input, useId,
 } from '@fluentui/react-components';
+import {SearchRegular} from '@fluentui/react-icons';
 
-import React from 'react';
-import {Card} from './model/Card.ts';
+import {Card} from './model/Card';
+import {LanguageInfo} from './model/LanguageInfo';
 
 import './Dictionary.css'
-import {LanguageInfo} from './model/LanguageInfo.ts';
 
 type DictionaryTableProps = {
   cards: Card[]
   currentLanguage: LanguageInfo
 }
 
-export const Dictionary: React.FunctionComponent<DictionaryTableProps> = ({cards, currentLanguage }) => {
+export const Dictionary: React.FunctionComponent<DictionaryTableProps> = ({cards, currentLanguage}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchTermInputId = useId("search-term-input");
+
+  const filteredCards = cards
+    .filter(card => card.en.includes(searchTerm) || card.word.includes(searchTerm))
+    .sort((c1, c2) => c1.en.localeCompare(c2.en));
+
   return (
-    <Table arial-label="Default table">
-      <TableHeader>
-        <TableRow>
-          <TableHeaderCell>English</TableHeaderCell>
-          <TableHeaderCell>{currentLanguage.fullName}</TableHeaderCell>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {cards.map((card) => (
-          <TableRow key={card.en}>
-            <TableCell>{card.en}</TableCell>
-            <TableCell className='hebrewFont'>{card.word}</TableCell>
+    <>
+      <div>
+        <Input id={searchTermInputId}
+               contentBefore={<SearchRegular />}
+               onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <span>Word count: {cards.length}</span>
+      </div>
+      <Table arial-label="Default table">
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell>English</TableHeaderCell>
+            <TableHeaderCell>{currentLanguage.fullName}</TableHeaderCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {filteredCards.map((card) => (
+            <TableRow key={card.en}>
+              <TableCell>{card.en}</TableCell>
+              <TableCell className='hebrewFont'>{card.word}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 };
