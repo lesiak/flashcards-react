7ee115132dc0ddce88f664fdb8e6a1f96bfdc608
lesiak <lesiak@outlook.com>
2023-08-09 17:48:40 +0200

    Read route segments in API

--- api/word-pronunciations/index.js
+++ api/word-pronunciations/index.js
@@ -1,16 +1,15 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const word = context.bindingData.word;
+    const lang = context.bindingData.lang;
+    var message = `word: ${word}, lang: ${lang}`;
 
     const todoItemResp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const todoItem = await todoItemResp.json();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage + JSON.stringify(todoItem)
+        body: message + JSON.stringify(todoItem)
     };
 }
