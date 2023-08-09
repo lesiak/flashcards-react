584c0751d4d3fc8e9ff90eb7b8189a165738a098
lesiak <lesiak@outlook.com>
2023-08-09 11:59:38 +0200

    test fetch api

--- api/message/index.js
+++ api/message/index.js
@@ -6,8 +6,11 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    const todoItemResp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const todoItem = await todoItemResp.json();
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: responseMessage + JSON.stringify(todoItem)
     };
 }
