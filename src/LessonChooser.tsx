8dadbfc80542450136e3e7b262ad2cf2d1a93096
lesiak <lesiak@outlook.com>
2025-01-14 21:29:10 +0100

    Grid Layout 3

--- src/LessonChooser.tsx
+++ src/LessonChooser.tsx
@@ -11,33 +11,15 @@ export const LessonChooser: React.FC<LessonChooserProps> = ({lessons, onLessonSe
   return (
     <div>
       <h1>Fluent UI Grid</h1>
-<div class="fl-grid">
-  <div class="fl-span12">
-    This should be 12 columns.
-  </div>
-  <div class="fl-span4">
-    This should be 4 columns.
-  </div>
-  <div class="fl-span8">
-    This should be 8 columns.
-  </div>
-  <div class="fl-span6c">
-    6 columns centered
-  </div>
-    <div class="fl-span8c">
-    8 columns centered
-  </div>
-
+      <div class="fl-grid">
       {lessons.map(lesson =>
         // onClick={() => setCurrentLesson(lesson)}
-        <div key={`${lesson.name}`} className="lesson-card fl-span8c" onClick={() => onLessonSelected(lesson)}>
+        <div key={`${lesson.name}`} className="lesson-card fl-span4" onClick={() => onLessonSelected(lesson)}>
           <LessonIndexCard lesson={lesson}/>
         </div>)
       }
-  
-  
-</div>
-</div>
+      </div>
+    </div>
   )
 }
 
